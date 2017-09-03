5c2837d4ee20b78e20b06ab0fe9909ab4ef41b61
snekavsb <31505096+snekavsb@users.noreply.github.com>
2017-09-03 17:53:28 +0530

    Create po.c

---
+++ po.c
@@ -0,0 +1,16 @@
+# include <stdio.h>
+#include<conio.h>
+void main
+{
+int i,n;
+scanf("%d",&n);
+printf("%d",n);
+if(n>=0)
+{
+printf("positive numbers")
+}
+else
+{
+printf("negative numbers")
+}
+}
